2f4b96c7a2d44bd5834cc688e50a0b9e78d7d0ad
Yannick Thibos <yannick.thibos@gmail.com>
2016-05-30 15:24:24 +0200

    Removed unused imports

--- src/main/java/be/vdab/services/BrouwerServiceImpl.java
+++ src/main/java/be/vdab/services/BrouwerServiceImpl.java
@@ -3,10 +3,7 @@ package be.vdab.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.transaction.annotation.Isolation;
-import org.springframework.transaction.annotation.Transactional;
 
 import be.vdab.entities.Brouwer;
 import be.vdab.repositories.BrouwerRepository;
